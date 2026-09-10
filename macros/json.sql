915384e51ac232288b6831a85f2b72f5c21556b2
Karan Manoharan <karanmanoharan@Karans-MacBook-Pro.local>
2026-09-10 10:11:19 +0100

    Add uv toolchain, DuckDB dev target, cross-adapter and CDC macros

--- macros/json.sql
+++
@@ -1,7 +0,0 @@
-{% macro as_variant(expression) -%}
-try_parse_json(to_varchar({{ expression }}))
-{%- endmacro %}
-
-{% macro json_text(expression, path) -%}
-cast(get_path({{ expression }}, '{{ path }}') as varchar)
-{%- endmacro %}
